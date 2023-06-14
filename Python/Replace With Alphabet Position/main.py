739080cfa3dde77fd84ab2e811ba309d37d7f329
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2023-06-14 00:10:47 -0400

    Added 'Replace With Alphabet Position' Kata in Python

---
+++ Python/Replace With Alphabet Position/main.py
@@ -0,0 +1,55 @@
+from solution import AlphabetPosition
+
+text = "Hello World"
+result = AlphabetPosition.alphabet_position(text)
+print(result)
+
+print(AlphabetPosition.alphabet_position(" j!}/-?:"));
+print(AlphabetPosition.alphabet_position("?)_b/,q("));
+print(AlphabetPosition.alphabet_position("(01:7co["));
+print(AlphabetPosition.alphabet_position("(pecthep"));
+print(AlphabetPosition.alphabet_position(")hy8:yv#"));
+print(AlphabetPosition.alphabet_position("}!g/$6h "));
+print(AlphabetPosition.alphabet_position("*.!@2%!("));
+print(AlphabetPosition.alphabet_position("/|2)<+$_"));
+print(AlphabetPosition.alphabet_position("/x9y62/z"));
+print(AlphabetPosition.alphabet_position("&?u7&@_h"));
+print(AlphabetPosition.alphabet_position("#rjf26wx"));
+print(AlphabetPosition.alphabet_position("%v0i#$1f"));
+print(AlphabetPosition.alphabet_position("^n%7_28b"));
+print(AlphabetPosition.alphabet_position("<csvc5b6"));
+print(AlphabetPosition.alphabet_position(">4@[{7+x"));
+print(AlphabetPosition.alphabet_position("$)^py>]v"));
+print(AlphabetPosition.alphabet_position("$^}[i%4x"));
+print(AlphabetPosition.alphabet_position("2!tw#^%="));
+print(AlphabetPosition.alphabet_position("2%s,lq;3"));
+print(AlphabetPosition.alphabet_position("21w;jo{4"));
+print(AlphabetPosition.alphabet_position("4.<esrvs"));
+print(AlphabetPosition.alphabet_position("4+$!.rey"));
+print(AlphabetPosition.alphabet_position("413x)!g]"));
+print(AlphabetPosition.alphabet_position("653/7%7+"));
+print(AlphabetPosition.alphabet_position("7f5f#z0&"));
+print(AlphabetPosition.alphabet_position("9?gwsh%<"));
+print(AlphabetPosition.alphabet_position("d))<bqax"));
+print(AlphabetPosition.alphabet_position("e[!o!:&r"));
+print(AlphabetPosition.alphabet_position("e936{oq0"));
+print(AlphabetPosition.alphabet_position("et$r!4?@"));
+print(AlphabetPosition.alphabet_position("gwfz96#<"));
+print(AlphabetPosition.alphabet_position("h5_}z{%>"));
+print(AlphabetPosition.alphabet_position("k;ur2   "));
+print(AlphabetPosition.alphabet_position("k.a,4}%|"));
+print(AlphabetPosition.alphabet_position("o-&u;/q/"));
+print(AlphabetPosition.alphabet_position("o9n&s-^@"));
+print(AlphabetPosition.alphabet_position("of8s@xuk"));
+print(AlphabetPosition.alphabet_position("pb$z ])j"));
+print(AlphabetPosition.alphabet_position("rvz:@&45"));
+print(AlphabetPosition.alphabet_position("t-^hnhl "));
+print(AlphabetPosition.alphabet_position("tdcxcaeb"));
+print(AlphabetPosition.alphabet_position("tg5a^b,."));
+print(AlphabetPosition.alphabet_position("um%ak4[w"));
+print(AlphabetPosition.alphabet_position("v2vs#;rz"));
+print(AlphabetPosition.alphabet_position("v8g-o#{>"));
+print(AlphabetPosition.alphabet_position("w}5#w_;}"));
+print(AlphabetPosition.alphabet_position("w$^(m[[/"));
+print(AlphabetPosition.alphabet_position("x?h?#c30"));
+print(AlphabetPosition.alphabet_position("x@@j[=f|"));
